5bb6d02f363273cbba56e5894832a9002c2d391e
ngabroger <arbination13@gmail.com>
2024-08-01 19:18:23 +0700

    update

--- pages/kasir.php
+++ pages/kasir.php
@@ -76,7 +76,14 @@ if (!isset($_SESSION['id']) || !isset($_SESSION['user_name']) || !isset($_SESSIO
             <span class="nav-link-text ms-1">Kelola</span>
           </a>
         </li>
-       
+        <li class="nav-item">
+          <a class="nav-link text-white " href="transaksi.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">receipt_long</i>
+            </div>
+            <span class="nav-link-text ms-1">Transaksi</span>
+          </a>
+        </li>
       </ul>
     </div>
     
